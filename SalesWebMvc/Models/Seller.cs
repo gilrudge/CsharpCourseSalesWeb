f3022d3d5f8dbb62eb56b3588a103a40b959edd6
Gil Rudge <gilblas07@hotmail.com>
2025-07-31 14:01:44 +0100

    Delete Seller

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -5,15 +5,15 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         public DateTime BirthDate { get; set; }
 
         public double BaseSalary { get; set; }
 
-        public Department Department { get; set; }
+        public Department? Department { get; set; }
 
         public int DepartmentId { get; set; }
 
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,5 +1,9 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
 {
@@ -14,13 +18,26 @@ namespace SalesWebMvc.Services
 
         public List<Seller> FindAll()
         {
-             return _context.Seller.ToList();
+             //return _context.Seller.ToList();
+            return [ .. _context.Seller];
         }
 
         public void Insert(Seller obj)
         {
             //obj.Department = _context.Department.First();
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int Id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == Id);
+        }
+
+        public void Remove(int Id)
+        {
+            var obj = _context.Seller.Find(Id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
